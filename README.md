150561c8dd93081391e0a52934c021f83b7a5b49
Smokingsnakes83 <smokingsnakes83@protonmail.com>
2024-05-10 20:03:28 -0300

    Ferbot's birth

---
+++ README.md
@@ -0,0 +1,13 @@
+# FerBot - Seu Assistente Virtual.
+
+O FerBot é um chatbot divertido e informativo, pronto para te ajudar com o que precisar! Se você busca informações sobre tecnologia, ciência, história, ou qualquer outro assunto, o FerBot está aqui para te guiar.
+Com o FerBot você pode:
+**Obter respostas rápidas e precisas para suas perguntas.**
+**Explorar diversos temas de forma interativa e envolvente.**
+**Aprender coisas novas de um jeito descontraído e informal.**
+# Como usar o FerBot:
+Basta digitar sua pergunta no campo indicado e o FerBot te responderá prontamente.
+Contribua com o projeto:
+Se você tem sugestões de funcionalidades, correções de bugs ou deseja colaborar, sinta-se à vontade para entrar em contato conosco.
+Mande brasa e bora conversar! 😉
+
---
+++ ferbot.py
@@ -0,0 +1,57 @@
+import google.generativeai as genai
+
+GOOGLE_API_KEY = 'AIzaSyDWqjYpW7J9OGmXgkAgNV-4GkAOr4F3u20'
+genai.configure(api_key=GOOGLE_API_KEY)
+
+gen_config = {
+    'candidate_count': 1,
+    'temperature': 0.5
+}
+
+safety_config = {
+    'harassment': 'block_none',
+    'hate': 'block_none',
+    'sexual': 'block_none',
+    'dangerous': 'block_none'
+}
+
+model = genai.GenerativeModel(model_name='gemini-1.0-pro',
+                              generation_config=gen_config,
+                              safety_settings=safety_config)
+
+chat = model.start_chat(history=[])
+
+print('\n\t\t\t❰F❱❰E❱❰R❱❰B❱❰O❱❰T❱\n')
+
+hello = ('Responda com uma saudação informal e despojada para o usuário, faça o uso de algumas gírias. Seu nome agora é FerBot, se' 'apresente brevemente ao usuário e cite algumas de suas habilidade e encoraje o usuário a lhe fazer perguntas dos temas mais variados')
+
+response = model.generate_content(hello)
+print(f'🤖 {response.text}')
+
+prompt = input('\n🤖 Faça a sua pergunta: ')
+
+if prompt == 'FerBot':
+    response = model.generate_content('Responda com poucas palavras como se alguém estisse te chamando, seja direto')
+    print(f'\n{response.text} \n')
+
+while prompt != 'bye' and prompt != 'BYE':
+  
+  if prompt == 'FerBot':
+    response = model.generate_content('Responda como se alguém estisse te chamando')
+    print(f'\n🤖 {response.text} \n')
+  else:   
+    response = chat.send_message(prompt + 'Seja objetivo nas respostas')
+    print(f'\nResposta: 🤖 {response.text} \n')
+  
+  for c in prompt:
+    print('*', end='')
+
+  print('\n')
+  prompt = input('🤖 Faça a sua pergunta: ')
+
+response = model.generate_content('Escreva uma breve frase de despedida')
+
+if chat.history != []:
+  print(f'\n🤖 {response.text} \n')
+else:  
+  print(f'\n🤖 Bye! Que pena que não conversamos\n')
